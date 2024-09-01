440b939a1e19d602470189d145a42df83993d8eb
gerumh.lincoln <qraztec@gmail.com>
2024-09-01 12:12:30 -0700

    Trying to set the correcl axios url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from "./context/AuthContext";
 
+
+axios.defaults.baseURL = 'http://34.209.241.12:8800/api';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
